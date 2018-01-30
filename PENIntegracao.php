646de5d42845d71cc4192d56a21ee645573f0783
Guilherme Andrade Del Cantoni <guilherme.cantoni@planejamento.gov.br>
2018-01-30 14:10:10 -0200

    Correção da indicação de número de versão do módulo

--- PENIntegracao.php
+++ PENIntegracao.php
@@ -9,7 +9,7 @@ class PENIntegracao extends SeiIntegracao {
     }
 
     public function getVersao() {
-        return '1.0.0';
+        return '1.1.0';
     }
 
     public function getInstituicao() {
