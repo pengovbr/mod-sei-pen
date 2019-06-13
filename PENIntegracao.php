dd532b9b620e54deff7125e7b79b6ee44ff252f7
Guilherme Andrade Del Cantoni <guilherme.cantoni@planejamento.gov.br>
2019-06-13 14:40:19 -0300

    Liberação de compatibilidade do módulo para a versão 3.1.1 do SEI

--- PENIntegracao.php
+++ PENIntegracao.php
@@ -2,7 +2,7 @@
 
 class PENIntegracao extends SeiIntegracao {
 
-    const COMPATIBILIDADE_MODULO_SEI = array('3.0.5', '3.0.6', '3.0.7', '3.0.8', '3.0.9', '3.0.11', '3.0.12', '3.0.13', '3.0.14', '3.0.15', '3.1.0');
+    const COMPATIBILIDADE_MODULO_SEI = array('3.0.5', '3.0.6', '3.0.7', '3.0.8', '3.0.9', '3.0.11', '3.0.12', '3.0.13', '3.0.14', '3.0.15', '3.1.0', '3.1.1');
 
     private static $strDiretorio;
 
