21a0ca295fb5dfad2095eb556f27003ac6b5b94b
Marcelo Linhares Castro <marlinhares@gmail.com>
2019-12-23 09:36:21 -0300

    Incrementando versao

--- PENIntegracao.php
+++ PENIntegracao.php
@@ -11,7 +11,7 @@ class PENIntegracao extends SeiIntegracao {
     }
 
     public function getVersao() {
-        return '1.2.5';
+        return '1.2.6';
     }
 
     public function getInstituicao() {
