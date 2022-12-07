21ea07686f00e4599a1d229bd48bbdae0ea68f0e
Lucas Vieira <lucasvieirasantos2@gmail.com>
2022-12-07 16:09:28 -0300

    fix: Melhoria na verificação de instalação do módulo

--- src/scripts/verifica_instalacao_modulo_pen.php
+++ src/scripts/verifica_instalacao_modulo_pen.php
@@ -3,7 +3,7 @@
 $dirSeiWeb = !defined("DIR_SEI_WEB") ? getenv("DIR_SEI_WEB") ?: __DIR__."/../../web" : DIR_SEI_WEB;
 require_once $dirSeiWeb . '/SEI.php';
 
-// Garante que código abaixo foi executado unicamente via linha de comando
+// Garante que cï¿½digo abaixo foi executado unicamente via linha de comando
 if ($argv && $argv[0] && realpath($argv[0]) === __FILE__) {
     InfraDebug::getInstance()->setBolLigado(true);
     InfraDebug::getInstance()->setBolDebugInfra(false);
@@ -22,21 +22,21 @@ if ($argv && $argv[0] && realpath($argv[0]) === __FILE__) {
 
         $objVerificadorInstalacaoRN = new VerificadorInstalacaoRN();
 
-        $fnPrint("INICIANDO VERIFICAÇÃO DA INSTALAÇÃO DO MÓDULO MOD-SEI-PEN:", 0);
+        $fnPrint("INICIANDO VERIFICAï¿½ï¿½O DA INSTALAï¿½ï¿½O DO Mï¿½DULO MOD-SEI-PEN:", 0);
 
         sleep(1);
         if($objVerificadorInstalacaoRN->verificarPosicionamentoScripts()){
-            $fnPrint("- Arquivos do módulo posicionados corretamente", 1);
+            $fnPrint("- Arquivos do mï¿½dulo posicionados corretamente", 1);
         }
 
         sleep(1);
         if($objVerificadorInstalacaoRN->verificarAtivacaoModulo()){
-            $fnPrint("- Módulo corretamente ativado no arquivo de configuracao do sistema", 1);
+            $fnPrint("- Mï¿½dulo corretamente ativado no arquivo de configuracao do sistema", 1);
         }
 
         sleep(1);
         if($objVerificadorInstalacaoRN->verificarArquivoConfiguracao()){
-            $fnPrint("- Parâmetros técnicos obrigatórios de integração atribuídos em ConfiguracaoModPEN.php", 1);
+            $fnPrint("- Parï¿½metros tï¿½cnicos obrigatï¿½rios de integraï¿½ï¿½o atribuï¿½dos em ConfiguracaoModPEN.php", 1);
         }        
 
         sleep(1);
@@ -46,31 +46,34 @@ if ($argv && $argv[0] && realpath($argv[0]) === __FILE__) {
 
         sleep(1);
         if($objVerificadorInstalacaoRN->verificarConexaoBarramentoPEN()){
-            $fnPrint("- Conexão com o Barramento de Serviços do PEN realizada com sucesso", 1);
+            $fnPrint("- Conexï¿½o com o Barramento de Serviï¿½os do PEN realizada com sucesso", 1);
         }
 
         sleep(1);
         if($objVerificadorInstalacaoRN->verificarAcessoPendenciasTramitePEN()){
-            $fnPrint("- Acesso aos dados do Comitê de Protocolo vinculado ao certificado realizado com sucesso", 1);
+            $fnPrint("- Acesso aos dados do Comitï¿½ de Protocolo vinculado ao certificado realizado com sucesso", 1);
         }
 
         sleep(1);
         if($objVerificadorInstalacaoRN->verificarConfiguracaoGearman()){
-            $fnPrint("- Conexão com o servidor de processamento de tarefas Gearman realizada com sucesso", 1);
+            $fnPrint("- Conexï¿½o com o servidor de processamento de tarefas Gearman realizada com sucesso", 1);
+        } else {
+            throw new ErrorException($fnPrint("- servidor identificados para o gearmand, tentando proceder com a conexão -", 0));
+            exit(1);
         }
 
         sleep(1);
         if($objVerificadorInstalacaoRN->verificarCompatibilidadeModulo()){
-            $fnPrint("- Verificada a compatibilidade do mod-sei-pen com a atual versão do SEI", 1);
+            $fnPrint("- Verificada a compatibilidade do mod-sei-pen com a atual versï¿½o do SEI", 1);
         }
 
         sleep(1);
         if($objVerificadorInstalacaoRN->verificarCompatibilidadeBanco()){
-            $fnPrint("- Base de dados do SEI corretamente atualizada com a versão atual do mod-sei-pen", 1);
+            $fnPrint("- Base de dados do SEI corretamente atualizada com a versï¿½o atual do mod-sei-pen", 1);
         }
 
         $fnPrint("", 0);
-        $fnPrint("** VERIFICAÇÃO DA INSTALAÇÃO DO MÓDULO MOD-SEI-PEN FINALIZADA COM SUCESSO **", 0);
+        $fnPrint("** VERIFICAï¿½ï¿½O DA INSTALAï¿½ï¿½O DO Mï¿½DULO MOD-SEI-PEN FINALIZADA COM SUCESSO **", 0);
 
         exit(0);
     } finally {
