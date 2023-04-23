000b31217f7b891a500ad7d79c71cf0994e2fe20
Guilherme Andrade Del Cantoni <guilhermeadc@gmail.com>
2023-04-23 19:22:56 -0300

    fix: erro de validação de hash em documento do tipo e-mail

--- src/rn/ProcessoEletronicoRN.php
+++ src/rn/ProcessoEletronicoRN.php
@@ -631,8 +631,10 @@ class ProcessoEletronicoRN extends InfraRN
 
       $cabecalho->urgente = $urgente;
       $cabecalho->motivoDaUrgencia = $motivoUrgencia;
-      $cabecalho->obrigarEnvioDeTodosOsComponentesDigitais = $enviarTodosDocumentos;
-
+      //Parâmetro abaixo foi descontinuado por falhas e substituido pelo enviarApenasComponentesDigitaisPendentes
+      //$cabecalho->obrigarEnvioDeTodosOsComponentesDigitais = $enviarTodosDocumentos;
+      $cabecalho->enviarApenasComponentesDigitaisPendentes = !$enviarTodosDocumentos;
+      
       $this->atribuirInformacoesAssunto($cabecalho, $dblIdProcedimento);
       $this->atribuirInformacoesModulo($cabecalho);
 
