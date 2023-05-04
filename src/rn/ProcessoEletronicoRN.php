5152d9a103d96110a25fc9041dc7e0513c674691
Michael Oliveira Ferreira <oliveiramichael@outlook.com>
2023-05-04 09:20:40 -0300

    mensagem atualizada

--- src/rn/ProcessoEletronicoRN.php
+++ src/rn/ProcessoEletronicoRN.php
@@ -557,7 +557,7 @@ class ProcessoEletronicoRN extends InfraRN
             $strDetalhes = str_replace(array("\n", "\r"), ' ', InfraString::formatarJavaScript($this->tratarFalhaWebService($e)));
             $detalhes = InfraString::formatarJavaScript($this->tratarFalhaWebService($e));
             if (strpos(strtolower($strMensagem), "hash de ao menos um componente digital não confere")) {
-                $strMensagem = $this->validarMudancaOrdemDocumentos($parametros->dblIdProcedimento, $strMensagem, $strDetalhes);
+                $strMensagem = $this->validarMudancaOrdemDocumentos($parametros->dblIdProcedimento, $strMensagem, $parametros);
             }
             throw new InfraException($strMensagem, $e, $strDetalhes);
         } catch (\Exception $e) {
@@ -594,23 +594,32 @@ class ProcessoEletronicoRN extends InfraRN
             $objProtocoloBD = new RelProtocoloProtocoloBD(BancoSEI::getInstance());
             $arrProtocolos = $objProtocoloBD->listar($objProtocoloDTO);
 
-            $msg = "";
+            # PROTOCOLO
+            $objProtocoloDTO = new ProtocoloDTO();
+            $objProtocoloDTO->setDblIdProtocolo($objAtividadeDTO->getDblIdProtocolo());
+            $objProtocoloDTO->setOrd('IdProtocolo', InfraDTO::$TIPO_ORDENACAO_ASC);
+            $objProtocoloDTO->retTodos();
+            $objProtocoloBD = new ProtocoloBD(BancoSEI::getInstance());
+            $protocolos = $objProtocoloBD->listar($objProtocoloDTO)[0];
+
+
+            $msg = "Não foi possível enviar o processo por meio '".$protocolos->getStrProtocoloFormatado()."' do Tramita.GOV.BR, em decorrência de alteração da ordem de um ou mais documentos na árvore do processo. A seguir, a lista dos documentos com ordem alterada:";
             foreach ($arrProtocolos as $index => $protocolo) {
                 if ($index != $protocolo->getNumSequencia()){
                     $documento = str_pad($protocolo->getDblIdProtocolo2(), 6, '0', STR_PAD_LEFT);
                     $pos = $index + 1;
                     $sequencia = $protocolo->getNumSequencia() + 1;
-                    $msg .= " <br />A ordem do documento $documento foi modificada na árvore do processo, mudando da posição $pos para a posição $sequencia.";
+                    $msg .= " A ordem do documento $documento foi modificada na árvore do processo, mudando da posição $pos para a posição $sequencia.";
                 }
             }
+            $msg .= "Sugere-se desfazer as alterações acima listadas antes de realizar nova tentativa de trâmite. Mantenha sempre a ordem original dos documentos de processos recebidos pelo Tramita.GOV.BR, uma vez que sua instrução foi realizada por outro órgão. Tenha em mente que qualquer alteração nessa ordem pode impedir um novo trâmite do processo.";
 
             if ($arrObjAtividadeDTO > 0) {
                 $strMensagem = str_replace(
                     'hash de ao menos um componente digital não confere',
-                    ' Possivelmente a falha ocorreu devido a mundaça de ordem do documento na árvore do processo.',
+                    $msg,
                     $strMensagem
                 );
-                $strMensagem.=$msg;
             }
         }
 
