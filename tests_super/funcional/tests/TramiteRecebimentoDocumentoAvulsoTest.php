8b51321e0a05fd3272c7d669b2c02516449c7931
higocavalcante <higodf@gmail.com>
2023-11-07 18:10:14 -0300

    chore: Adição de cenário para devolução de documento avulso

--- tests_super/funcional/tests/TramiteRecebimentoDocumentoAvulsoTest.php
+++ tests_super/funcional/tests/TramiteRecebimentoDocumentoAvulsoTest.php
@@ -11,9 +11,15 @@ class TramiteRecebimentoDocumentoAvulsoTest extends CenarioBaseTestCase
     const CONTEUDO_DOCUMENTO_B = "arquivo_pequeno_B.pdf";
     const CONTEUDO_DOCUMENTO_C = "arquivo_pequeno_C.pdf";
 
-    protected $remetente;
-    protected $destinatario;
     protected $servicoPEN;
+    public static $remetente;
+    public static $destinatario;
+    public static $processoTeste;
+    public static $documentoTeste1;
+    public static $documentoTeste2;
+    public static $documentoTeste3;
+    public static $documentoTeste4;
+    public static $documentoTeste5;
 
     /**
      * Teste de recebimento dedocumento avulso com 2 componentes digitais
@@ -27,12 +33,12 @@ class TramiteRecebimentoDocumentoAvulsoTest extends CenarioBaseTestCase
         parent::setup();
 
         // Carregar contexto de testes e dados sobre certificado digital
-        $this->remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_B);
-        $this->destinatario = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
+        self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_B);
+        self::$destinatario = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
 
         // Instanciar objeto de teste utilizando o BeSimpleSoap
-        $localCertificado = $this->remetente['LOCALIZACAO_CERTIFICADO_DIGITAL'];
-        $senhaCertificado = $this->remetente['SENHA_CERTIFICADO_DIGITAL'];
+        $localCertificado = self::$remetente['LOCALIZACAO_CERTIFICADO_DIGITAL'];
+        $senhaCertificado = self::$remetente['SENHA_CERTIFICADO_DIGITAL'];
         $this->servicoPEN = $this->instanciarApiDeIntegracao($localCertificado, $senhaCertificado);
     }
 
@@ -44,10 +50,10 @@ class TramiteRecebimentoDocumentoAvulsoTest extends CenarioBaseTestCase
     public function test_recebimento_documento_avulso()
     {
         // Simular um trâmite chamando a API do Barramento diretamente
-        $documentoTeste = $this->gerarDadosDocumentoExternoTeste($this->remetente, array(self::CONTEUDO_DOCUMENTO_A));
+        self::$documentoTeste1 = $this->gerarDadosDocumentoExternoTeste(self::$remetente, array(self::CONTEUDO_DOCUMENTO_A));
 
-        $metadadosDocumentoTeste = $this->construirMetadadosDocumentoTeste($documentoTeste);
-        $novoTramite = $this->enviarMetadadosDocumento($this->servicoPEN, $this->remetente, $this->destinatario, $metadadosDocumentoTeste);
+        $metadadosDocumentoTeste = $this->construirMetadadosDocumentoTeste(self::$documentoTeste1);
+        $novoTramite = $this->enviarMetadadosDocumento($this->servicoPEN, self::$remetente, self::$destinatario, $metadadosDocumentoTeste);
         $this->enviarComponentesDigitaisDoTramite($this->servicoPEN, $novoTramite, $metadadosDocumentoTeste);
         $reciboTramite = $this->receberReciboEnvio($this->servicoPEN, $novoTramite);
         $this->atualizarTramitesPEN(true,false);
@@ -55,9 +61,86 @@ class TramiteRecebimentoDocumentoAvulsoTest extends CenarioBaseTestCase
         //Verificar recebimento de novo processo administrativo contendo documento avulso enviado
         $this->assertNotNull($novoTramite);
         $this->assertNotNull($reciboTramite);
-        $this->realizarValidacaoRecebimentoDocumentoAvulsoNoDestinatario($documentoTeste, $this->destinatario);
+        self::$processoTeste = $this->realizarValidacaoRecebimentoDocumentoAvulsoNoDestinatario(self::$documentoTeste1, self::$destinatario);
     }
 
+    /**
+     * Teste de trâmite externo de processo com devolução para a mesma unidade de origem
+     *
+     * @group envio
+     * @large
+     *
+     * @depends test_recebimento_documento_avulso
+     *
+     * @return void
+     */
+    public function test_devolucao_processo_para_origem()
+    {
+        // Configuração do dados para teste do cenário
+        self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
+        self::$destinatario = $this->definirContextoTeste(CONTEXTO_ORGAO_B);
+        self::$documentoTeste2 = $this->gerarDadosDocumentoInternoTeste(self::$remetente);
+        self::$documentoTeste3 = $this->gerarDadosDocumentoExternoTeste(self::$remetente);
+
+        $documentos = array(self::$documentoTeste2, self::$documentoTeste3);
+        $this->realizarTramiteExternoComValidacaoNoRemetente(self::$processoTeste, $documentos, self::$remetente, self::$destinatario);
+    }
+
+    /**
+     * Teste de verificação do correto recebimento do processo no destino
+     *
+     * @group verificacao_recebimento
+     * @large
+     *
+     * @depends test_devolucao_processo_para_origem
+     *
+     * @return void
+     */
+    public function test_verificar_recebimento_processo_destino()
+    {
+        self::$destinatario = $this->definirContextoTeste(CONTEXTO_ORGAO_B);
+        $documentos = array(self::$documentoTeste1, self::$documentoTeste2, self::$documentoTeste3);
+        $this->realizarValidacaoRecebimentoProcessoNoDestinatario(self::$processoTeste, $documentos, self::$destinatario);
+    }
+
+    /**
+     * Teste de trâmite externo de processo com devolução para a mesma unidade de origem
+     *
+     * @group envio
+     * @large
+     *
+     * @depends test_verificar_recebimento_processo_destino
+     *
+     * @return void
+     */
+    public function test_devolucao_processo_para_origem_documento_avulso()
+    {
+        // Configuração do dados para teste do cenário
+        self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_B);
+        self::$destinatario = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
+        self::$documentoTeste4 = $this->gerarDadosDocumentoInternoTeste(self::$remetente);
+        self::$documentoTeste5 = $this->gerarDadosDocumentoExternoTeste(self::$remetente);
+
+        $documentos = array(self::$documentoTeste4, self::$documentoTeste5);
+        $this->realizarTramiteExternoComValidacaoNoRemetente(self::$processoTeste, $documentos, self::$remetente, self::$destinatario);
+    }
+
+    /**
+     * Teste de verificação do correto recebimento do processo no destino
+     *
+     * @group verificacao_recebimento
+     * @large
+     *
+     * @depends test_devolucao_processo_para_origem_documento_avulso
+     *
+     * @return void
+     */
+    public function test_verificar_recebimento_processo_destino_documento_avulso()
+    {
+        self::$destinatario = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
+        $documentos = array(self::$documentoTeste1, self::$documentoTeste2, self::$documentoTeste3, self::$documentoTeste4, self::$documentoTeste5);
+        $this->realizarValidacaoRecebimentoProcessoNoDestinatario(self::$processoTeste, $documentos, self::$destinatario);
+    }
 
     private function receberReciboEnvio($servicoPEN, $novoTramite)
     {
