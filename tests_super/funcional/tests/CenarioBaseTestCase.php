81b8c434b4216c811012899b56b8573c251bb3d3
Glaydson Rodrigues <glaydson012@gmail.com>
2024-02-09 16:46:20 -0300

    chore: remover comentário

--- tests_super/funcional/tests/CenarioBaseTestCase.php
+++ tests_super/funcional/tests/CenarioBaseTestCase.php
@@ -580,7 +580,7 @@ class CenarioBaseTestCase extends Selenium2TestCase
         $this->frame(null);
         $this->byId("txtInfraPesquisarMenu")->value(utf8_encode('Processos Tramitados Externamente'));
         $this->byLinkText("Processos Tramitados Externamente")->click();
-        // $this->assertEquals($deveExistir, $this->paginaProcessosTramitadosExternamente->contemProcesso($protocolo));
+        $this->assertEquals($deveExistir, $this->paginaProcessosTramitadosExternamente->contemProcesso($protocolo));
     }
 
     protected function validarProcessoRejeitado()
