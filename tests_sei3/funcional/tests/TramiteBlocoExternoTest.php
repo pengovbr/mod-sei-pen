56d33995706289b32cc56371563f0894dc962efe
Glaydson Rodrigues <glaydson012@gmail.com>
2024-03-06 15:15:35 -0300

    chore: adapatar teste funcionao de tramite para sei3

--- tests_sei3/funcional/tests/TramiteBlocoExternoTest.php
+++ tests_sei3/funcional/tests/TramiteBlocoExternoTest.php
@@ -9,6 +9,11 @@ class TramiteBlocoExternoTest extends CenarioBaseTestCase
     public static $remetente;
     public static $destinatario;
     public static $penOrgaoExternoId;
+    public static $processoTeste;
+    public static $documentoTeste;
+    public static $protocoloTeste;
+    public static $bloco;
+    public static $documentoTeste1;
 
     function setUp(): void 
     {
@@ -16,17 +21,12 @@ class TramiteBlocoExternoTest extends CenarioBaseTestCase
         self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
         self::$destinatario = $this->definirContextoTeste(CONTEXTO_ORGAO_B);
 
-        $penMapUnidadesFixture = new PenMapUnidadesFixture(CONTEXTO_ORGAO_A, [
-            'id' => self::$remetente['ID_ESTRUTURA'],
-            'sigla' => self::$remetente['SIGLA_ESTRUTURA'],
-            'nome' => self::$remetente['NOME_UNIDADE']
-        ]);
-        $penMapUnidadesFixture->gravar();
-
     }
 
-    public function teste_tramite_bloco_externo()
+    public function teste_incluir_processo_em_bloco()
     {
+        self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
+
         $this->acessarSistema(
             self::$remetente['URL'],
             self::$remetente['SIGLA_UNIDADE'],
@@ -34,12 +34,80 @@ class TramiteBlocoExternoTest extends CenarioBaseTestCase
             self::$remetente['SENHA']
         );
 
+        $this->cadastrarBlocoDeTramite();
+        $this->paginaBase->navegarParaControleProcesso();
+        $this->cadastrarProcessos();
+        $this->paginaCadastrarProcessoEmBloco->navegarListagemBlocoDeTramite();
+        $this->paginaBase->navegarParaControleProcesso();
+        // $this->paginaTramiteEmBloco->selecionarProcessos([self::$protocoloTeste]);
+        $this->paginaTramiteEmBloco->selecionarProcessos(self::$protocoloTeste);
+        $this->paginaTramiteEmBloco->selecionarTramiteEmBloco();
+        $this->paginaTramiteEmBloco->clicarSalvar();
+
+        sleep(2);
+        $mensagem = $this->paginaTramiteEmBloco->buscarMensagemAlerta();
+
+        $this->assertStringContainsString(
+            utf8_encode('Processo(s) incluído(s) com sucesso no bloco'),
+            $mensagem
+        );
+
+        $this->paginaBase->navegarParaControleProcesso();
         $this->paginaCadastrarProcessoEmBloco->navegarListagemBlocoDeTramite();
         $this->paginaCadastrarProcessoEmBloco->bntTramitarBloco();
         $this->paginaCadastrarProcessoEmBloco->tramitarProcessoExternamente(
             self::$destinatario['REP_ESTRUTURAS'], self::$destinatario['NOME_UNIDADE'],
             self::$destinatario['SIGLA_UNIDADE_HIERARQUIA'], false
         );
-        sleep(10);
+
+        // $this->paginaBase->sairSistema();
+    }
+
+        /**
+     * Cadastra o bloco de tramite
+     * @return void
+     */
+    private function cadastrarBlocoDeTramite()
+    {
+
+        // Configuração do dados para teste do cenário
+        $this->paginaCadastrarProcessoEmBloco->navegarListagemBlocoDeTramite();
+        $this->paginaCadastrarProcessoEmBloco->novoBlocoDeTramite();
+        $this->paginaCadastrarProcessoEmBloco->criarNovoBloco();
+        $this->paginaCadastrarProcessoEmBloco->btnSalvar();
+
+        sleep(1);
+        $mensagemRetornoAlert = $this->paginaCadastroOrgaoExterno->buscarMensagemAlerta();
+        $menssagemValidacao = utf8_encode('Bloco de Trâmite externo criado com sucesso!');
+
+        $this->assertStringContainsString(
+            $menssagemValidacao,
+            $mensagemRetornoAlert
+        );
+    }
+
+    /**
+     * Cadastra o bloco de tramite
+     * @return void
+     */
+    private function cadastrarProcessos()
+    {
+        self::$processoTeste = $this->gerarDadosProcessoTeste(self::$remetente);
+        self::$protocoloTeste = $this->cadastrarProcesso(self::$processoTeste);
+
+        self::$documentoTeste1 = $this->gerarDadosDocumentoInternoTeste(self::$remetente);
+        // self::$documentoTeste2 = $this->gerarDadosDocumentoExternoTeste(self::$remetente);
+
+        // $documentos = array(self::$documentoTeste1, self::$documentoTeste2);
+
+        $documentosTeste = array_key_exists('TIPO', self::$documentoTeste1) ? array(self::$documentoTeste1) : self::$documentoTeste1;
+        foreach ($documentosTeste as $doc) {
+            if ($doc['TIPO'] == 'G') {
+                $this->cadastrarDocumentoInterno($doc);
+                $this->assinarDocumento(self::$remetente['ORGAO'], self::$remetente['CARGO_ASSINATURA'], self::$remetente['SENHA']);
+            } else if ($doc['TIPO'] == 'R') {
+                $this->cadastrarDocumentoExterno($doc);
+            }
+        }
     }
 }
