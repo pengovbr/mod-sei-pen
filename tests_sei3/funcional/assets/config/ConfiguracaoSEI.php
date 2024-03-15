ea2bfc31932b92326701c6a315cae28ab0e6bba8
Marcelo L Castro <marlinhares@gmail.com>
2024-03-15 19:21:00 -0300

    Ajustes gerais para o sei3. Docker-compose e env

--- tests_sei3/funcional/assets/config/ConfiguracaoSEI.php
+++ tests_sei3/funcional/assets/config/ConfiguracaoSEI.php
@@ -13,94 +13,97 @@ class ConfiguracaoSEI extends InfraConfiguracao  {
 
 	public function getArrConfiguracoes(){
 		return array(
-
 			'SEI' => array(
-				'URL' => getenv('SEI_HOST_URL').'/sei',
+				'URL' => getenv('HOST_URL').'/sei',
 				'Producao' => false,
 				'RepositorioArquivos' => '/var/sei/arquivos',
-				'Modulos' => array(
-					"PENIntegracao" => "pen",
-					)
+
+				'DigitosDocumento' => 7,
+				'NumLoginUsuarioExternoSemCaptcha' => 3,
+				'TamSenhaUsuarioExterno' => 8,
+				'DebugWebServices' => 2,
+				'MaxMemoriaPdfGb' => 4,
+                'Modulos' => array(
+                    "PENIntegracao" => "pen",
+                )
+			),
+
+			'SessaoSEI' => array(
+				'SiglaOrgaoSistema' => 'ABC',
+				'SiglaSistema' => 'SEI',
+				'PaginaLogin' => getenv('HOST_URL') . '/sip/login.php',
+				'SipWsdl' => getenv('HOST_URL') . '/sip/controlador_ws.php?servico=wsdl',
+				'https' => false
 			),
 
 			'PaginaSEI' => array(
 				'NomeSistema' => 'SEI',
 				'NomeSistemaComplemento' => SEI_VERSAO,
-				'LogoMenu' => ''),
+				'LogoMenu' => '',
+				'OrgaoTopoJanela' => 'S',
+			),
 
-				'SessaoSEI' => array(
-					'SiglaOrgaoSistema' => 'ABC',
-					'SiglaSistema' => 'SEI',
-					'PaginaLogin' => getenv('SEI_HOST_URL').'/sip/login.php',
-					'SipWsdl' => 'http://localhost/sip/controlador_ws.php?servico=wsdl',
-					'https' => false
-				),
+			'BancoSEI'  => array(
+				'Servidor' => getenv('DATABASE_HOST'),
+				'Porta' => getenv('DATABASE_PORT'),
+				'Banco' => getenv('SEI_DATABASE_NAME'),
+				'Usuario' => getenv('SEI_DATABASE_USER'),
+				'Senha' => getenv('SEI_DATABASE_PASSWORD'),
+				'UsuarioScript' => getenv('SEI_DATABASE_USER_SCRIPT'),
+				'SenhaScript' => getenv('SEI_DATABASE_PASSWORD_SCRIPT'),
+				'Tipo' => getenv('DATABASE_TYPE'), //MySql, SqlServer ou Oracle
+				'PesquisaCaseInsensitive' => false,
+			),
 
-				'BancoSEI'  => array(
-						'Servidor' => 'mysql',
-						'Porta' => '3306',
-						'Banco' => 'sei',
-						'Usuario' => 'sei_user',
-						'Senha' => 'sei_user',
-						'UsuarioScript' => 'sei_user',
- 						'SenhaScript' => 'sei_user',
-						'Tipo' => 'MySql'),
+			'BancoAuditoriaSEI'  => array(
+				'Servidor' => 'mysql',
+				'Porta' => '3306',
+				'Banco' => 'sei',
+				'Usuario' => 'sei_user',
+				'Senha' => 'sei_user',
+				'Tipo' => 'MySql', //MySql, SqlServer ou Oracle
+			),
 
-					// 'BancoSEI'  => array(
-					// 	'Servidor' => 'oracle',
-					// 	'Porta' => '1521',
-					// 	'Banco' => 'sei',
-					// 	'Usuario' => 'sei',
-					// 	'Senha' => 'sei_user',
-					// 	'UsuarioScript' => 'sei',
-					// 	'SenhaScript' => 'sei_user',
-					// 	'Tipo' => 'Oracle'), //MySql, SqlServer ou Oracle
+			'CacheSEI' => array(
+				'Servidor' => 'memcached',
+				'Porta' => '11211',
+				'Timeout' => 2,
+				'Tempo' => 3600,
+			),
 
-					//   'BancoSEI'  => array(
-					//           'Servidor' => 'sqlserver',
-					//           'Porta' => '1433',
-					//           'Banco' => 'sei',
-					//           'Usuario' => 'sei_user',
-					//           'Senha' => 'sei_user',
-					//           'UsuarioScript' => 'sei_user',
-					//           'SenhaScript' => 'sei_user',
-					//           'Tipo' => 'SqlServer'), //MySql, SqlServer ou Oracle
+			'Solr' => array(
+				'Servidor' => 'http://solr:8983/solr',
+				'CoreProtocolos' => 'sei-protocolos',
+				'CoreBasesConhecimento' => 'sei-bases-conhecimento',
+				'CorePublicacoes' => 'sei-publicacoes',
+				'TempoCommitProtocolos' => 300,
+				'TempoCommitBasesConhecimento' => 60,
+				'TempoCommitPublicacoes' => 60,
+			),
 
-							'CacheSEI' => array('Servidor' => 'memcached',
-							'Porta' => '11211'),
+			'JODConverter' => array(
+				'Servidor' => 'http://jod/converter/service'
+			),
 
-							'JODConverter' => array('Servidor' => 'http://jod:8080/converter/service'),
+			'HostWebService' => array(
+				'Sip' => array('*'),
+				'Publicacao' => array('*'),
+				'Ouvidoria' => array('*'),
+			),
 
-							'Edoc' => array('Servidor' => 'http://[Servidor .NET]'),
-
-							'Solr' => array(
-								'Servidor' => 'http://solr:8983/solr',
-								'CoreProtocolos' => 'sei-protocolos',
-								'CoreBasesConhecimento' => 'sei-bases-conhecimento',
-								'CorePublicacoes' => 'sei-publicacoes'),
-
-								'HostWebService' => array(
-									'Edoc' => array('[Servidor .NET]'),
-									'Sip' => array('*'), //Referências (IP e nome na rede) de todas as máquinas que executam o SIP.
-									'Publicacao' => array('*'), //Referências (IP e nome na rede) das máquinas de veículos de publicação externos cadastrados no SEI.
-									'Ouvidoria' => array('*'), //Referências (IP e nome na rede) da máquina que hospeda o formulário de Ouvidoria personalizado. Se utilizar o formulário padrão do SEI, então configurar com as máquinas dos nós de aplicação do SEI.
-								),
-
-								'InfraMail' => array(
-									'Tipo' => '2', //1 = sendmail (neste caso não é necessário configurar os atributos abaixo), 2 = SMTP
-									'Servidor' => 'smtp',
-									'Porta' => '1025',
-									'Codificacao' => '8bit', //8bit, 7bit, binary, base64, quoted-printable
-									'MaxDestinatarios' => 999, //numero maximo de destinatarios por mensagem
-									'MaxTamAnexosMb' => 999, //tamanho maximo dos anexos em Mb por mensagem
-									'Seguranca' => '', //TLS, SSL ou vazio
-									'Autenticar' => false, //se true então informar Usuario e Senha
-									'Usuario' => '',
-									'Senha' => '',
-									'Protegido' => 'desenv@instituicao.gov.br' //campo usado em desenvolvimento, se tiver um email preenchido entao todos os emails enviados terao o destinatario ignorado e substituído por este valor (evita envio incorreto de email)
-									)
-								);
-							}
-						}
-
-
+			'InfraMail' => array(
+				'Tipo' => '2',
+				'Servidor' => 'smtp',
+				'Porta' => '1025',
+				'Codificacao' => '8bit',
+				'MaxDestinatarios' => 999,
+				'MaxTamAnexosMb' => 999,
+				'Autenticar' => false,
+				'Usuario' => '',
+				'Senha' => '',
+				'Seguranca' => '', //TLS, SSL ou vazio
+				'Protegido' => 'desenv@instituicao.gov.br',
+			),
+		);
+	}
+}
