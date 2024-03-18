e61e27b945f33ebcb00c64d6e8bf2f3ea16f4fab
Marcelo Linhares Castro <marlinhares@gmail.com>
2024-03-18 10:10:17 -0300

    Feat/449 SEI3 com Oracle e SqlServer (#452)
    * Ajustes para o ecossistema SEI3
    * Ecossistema agora nos mesmos moldes do sei4
    * Ecossistema agora suporta Oracle e Sqlserver

--- tests_sei3/funcional/assets/config/ConfiguracaoSip.php
+++ tests_sei3/funcional/assets/config/ConfiguracaoSip.php
@@ -2,84 +2,87 @@
 
 class ConfiguracaoSip extends InfraConfiguracao  {
 
- 	private static $instance = null;
+	private static $instance = null;
 
- 	public static function getInstance(){
- 	  if (ConfiguracaoSip::$instance == null) {
- 	    ConfiguracaoSip::$instance = new ConfiguracaoSip();
- 	  }
- 	  return ConfiguracaoSip::$instance;
- 	}
+	public static function getInstance(){
+		if (ConfiguracaoSip::$instance == null) {
+			ConfiguracaoSip::$instance = new ConfiguracaoSip();
+		}
+		return ConfiguracaoSip::$instance;
+	}
 
- 	public function getArrConfiguracoes(){
- 	  return array(
- 	      'Sip' => array(
- 	          'URL' => getenv('SEI_HOST_URL').'/sip',
- 	          'Producao' => false),
+	public function getArrConfiguracoes(){
+		return array(
+			'Sip' => array(
+				'URL' => getenv('HOST_URL').'/sip',
+				'Producao' => false,
+				'NumLoginSemCaptcha' => 3,
+				'TempoLimiteValidacaoLogin' => 60,
+				'Modulos' => array(
+					//'ABCExemploIntegracao' => 'abc/exemplo',
+				),
+			),
 
- 	      'PaginaSip' => array('NomeSistema' => 'SIP'),
+			'PaginaSip' => array(
+				'NomeSistema' => 'SIP',
+				'NomeSistemaComplemento' => '',
+			),
 
- 	      'SessaoSip' => array(
- 	          'SiglaOrgaoSistema' => 'ABC',
- 	          'SiglaSistema' => 'SIP',
- 	          'PaginaLogin' => getenv('SEI_HOST_URL').'/sip/login.php',
- 	          'SipWsdl' => 'http://localhost/sip/controlador_ws.php?servico=wsdl',
- 	          'https' => false),
+			'SessaoSip' => array(
+				'SiglaOrgaoSistema' => 'ABC',
+				'SiglaSistema' => 'SIP',
+				'PaginaLogin' => getenv('HOST_URL') . '/sip/login.php',
+				'SipWsdl' => getenv('HOST_URL') . '/sip/controlador_ws.php?servico=wsdl',
+				'https' => false
+			),
 
- 	      'BancoSip'  => array(
- 	          'Servidor' => 'mysql',
- 	          'Porta' => '3306',
- 	          'Banco' => 'sip',
- 	          'Usuario' => 'sip_user',
- 	          'Senha' => 'sip_user',
- 	          'UsuarioScript' => 'sip_user',
- 	          'SenhaScript' => 'sip_user',
- 	          'Tipo' => 'MySql'), //MySql, SqlServer ou Oracle
+			'BancoSip'  => array(
+				'Servidor' => getenv('DATABASE_HOST'),
+				'Porta' => getenv('DATABASE_PORT'),
+				'Banco' => getenv('SIP_DATABASE_NAME'),
+				'Usuario' => getenv('SIP_DATABASE_USER'),
+				'Senha' => getenv('SIP_DATABASE_PASSWORD'),
+				'UsuarioScript' => getenv('SIP_DATABASE_USER_SCRIPT'),
+				'SenhaScript' => getenv('SIP_DATABASE_PASSWORD_SCRIPT'),
+				'Tipo' => getenv('DATABASE_TYPE'), //MySql, SqlServer ou Oracle
+				'PesquisaCaseInsensitive' => false,
+			),
 
-	    //   'BancoSip'  => array(
-	    //       'Servidor' => 'oracle',
-	    //       'Porta' => '1521',
-	    //       'Banco' => 'sip',
-	    //       'Usuario' => 'sip',
-	    //       'Senha' => 'sip_user',
-	    //       'UsuarioScript' => 'sip',
-	    //       'SenhaScript' => 'sip_user',
-	    //       'Tipo' => 'Oracle'), //MySql, SqlServer ou Oracle
+			'BancoAuditoriaSip'  => array(
+				'Servidor' => 'mysql',
+				'Porta' => '3306',
+				'Banco' => 'sip',
+				'Usuario' => 'sip_user',
+				'Senha' => 'sip_user',
+				'Tipo' => 'MySql', //MySql, SqlServer ou Oracle
+			),
 
-	    //   'BancoSip'  => array(
-	    //       'Servidor' => 'sqlserver',
-	    //       'Porta' => '1433',
-	    //       'Banco' => 'sip',
-	    //       'Usuario' => 'sip_user',
-	    //       'Senha' => 'sip_user',
-	    //       'UsuarioScript' => 'sip_user',
-	    //       'SenhaScript' => 'sip_user',
-	    //       'Tipo' => 'SqlServer'), //MySql, SqlServer ou Oracle
+			'CacheSip' => array(
+				'Servidor' => 'memcached',
+				'Porta' => '11211',
+				'Timeout' => 2,
+				'Tempo' => 3600,
+			),
 
-
-
-				'CacheSip' => array('Servidor' => 'memcached',
-						                'Porta' => '11211'),
-
- 	      'HostWebService' => array(
- 	          'Replicacao' => array('*'), //endereço ou IP da máquina que implementa o serviço de replicação de usuários
- 	          'Pesquisa' => array('*'), //endereços/IPs das máquinas do SEI
- 	          'Autenticacao' => array('*')), //endereços/IPs das máquinas do SEI
+			'HostWebService' => array(
+				'Replicacao' => array('*'),
+				'Pesquisa' => array('*'),
+				'Autenticacao' => array('*')
+			),
 
 				'InfraMail' => array(
-						'Tipo' => '2', //1 = sendmail (neste caso não é necessário configurar os atributos abaixo), 2 = SMTP
-						'Servidor' => 'smtp',
-						'Porta' => '1025',
-						'Codificacao' => '8bit', //8bit, 7bit, binary, base64, quoted-printable
-						'MaxDestinatarios' => 999, //numero maximo de destinatarios por mensagem
-						'MaxTamAnexosMb' => 999, //tamanho maximo dos anexos em Mb por mensagem
-						'Seguranca' => '', //TLS, SSL ou vazio
-						'Autenticar' => false, //se true então informar Usuario e Senha
-						'Usuario' => '',
-						'Senha' => '',
-						'Protegido' => 'desenv@instituicao.gov.br' //campo usado em desenvolvimento, se tiver um email preenchido entao todos os emails enviados terao o destinatario ignorado e substituído por este valor (evita envio incorreto de email)
-				)
- 	  );
- 	}
-}
-
+					'Tipo' => '2',
+					'Servidor' => 'smtp',
+					'Porta' => '1025',
+					'Codificacao' => '8bit',
+					'MaxDestinatarios' => 999,
+					'MaxTamAnexosMb' => 999,
+					'Seguranca' => '', //TLS, SSL ou vazio
+					'Autenticar' => false,
+					'Usuario' => '',
+					'Senha' => '',
+					'Protegido' => 'desenv@instituicao.gov.br'
+					)
+				);
+			}
+		}
