9e148cda6e46c2091ce0deb56b6052371fd7f444
mateussbh <mateusbh@gmail.com>
2024-03-26 19:56:30 -0300

    chore: alterando grupo de teste do teste TramiteProcessoContendoDocumentoExternoMesmoOrgaoTest.php

--- tests_sei4/funcional/tests/TramiteProcessoContendoDocumentoExternoMesmoOrgaoTest.php
+++ tests_sei4/funcional/tests/TramiteProcessoContendoDocumentoExternoMesmoOrgaoTest.php
@@ -2,7 +2,7 @@
 
 /**
  * Execution Groups
- * @group execute_parallel_group1
+ * @group execute_alone_group6
  */
 class TramiteProcessoContendoDocumentoExternoMesmoOrgaoTest extends CenarioBaseTestCase
 {
