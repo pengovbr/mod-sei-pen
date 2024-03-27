d56e57abf352403faf6d5ab341629497c7bfeb6c
Paul Richard <paulrpm@gmail.com>
2024-03-27 08:59:13 -0300

    refactor: revertendo valor de constante

--- src/PENIntegracao.php
+++ src/PENIntegracao.php
@@ -7,7 +7,7 @@ class PENIntegracao extends SeiIntegracao
 {
   const VERSAO_MODULO = VERSAO_MODULO_PEN;
   const PARAMETRO_VERSAO_MODULO_ANTIGO = 'PEN_VERSAO_MODULO_SEI';
-  const PARAMETRO_VERSAO_MODULO = 'VERSAO_MODULO_INTEGRACAO_TRAMITA';
+  const PARAMETRO_VERSAO_MODULO = 'VERSAO_MODULO_PEN';
 
   private static $instance = null;
 
