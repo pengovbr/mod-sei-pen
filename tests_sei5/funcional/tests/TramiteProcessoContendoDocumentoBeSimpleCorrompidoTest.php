6b4bfa10d5d5bfc2d67d17ef6de7759aee10f790
Marcelo L Castro <marlinhares@gmail.com>
2024-05-27 23:39:40 -0300

    docker-compose, Oracle, Sqlserver, Postgres, Mysql para o ecossistema do SEI5. Correcoes diversas para php8

--- tests_sei5/funcional/tests/TramiteProcessoContendoDocumentoBeSimpleCorrompidoTest.php
+++ tests_sei5/funcional/tests/TramiteProcessoContendoDocumentoBeSimpleCorrompidoTest.php
@@ -3,7 +3,7 @@
 /**
  *
  * Execution Groups
- * @group execute_parallel_with_two_group1
+ * @group execute_alone_group9
  */
 class TramiteProcessoContendoDocumentoBeSimpleCorrompidoTest extends CenarioBaseTestCase
 {
--- tests_sei5/funcional/tests/TramiteProcessoRestritoTest.php
+++ tests_sei5/funcional/tests/TramiteProcessoRestritoTest.php
@@ -3,7 +3,7 @@
 /**
  *
  * Execution Groups
- * @group execute_parallel_group1
+ * @group execute_alone_group8
  */
 class TramiteProcessoRestritoTest extends CenarioBaseTestCase
 {
