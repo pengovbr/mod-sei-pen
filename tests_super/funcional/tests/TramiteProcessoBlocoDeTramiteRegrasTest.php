494cc4f8d7d50c549e2b3076731fb03b6f5eff96
Paul Richard Pereira Martins dos Anjos <paul.anjos@gestao.gov.br>
2024-10-04 11:45:20 -0300

    feat: atualizado testes com cenario extra de processo sem documento; limpeza de codigo

--- tests_super/funcional/tests/TramiteProcessoBlocoDeTramiteRegrasTest.php
+++ tests_super/funcional/tests/TramiteProcessoBlocoDeTramiteRegrasTest.php
@@ -10,9 +10,6 @@ use Tests\Funcional\Sei\Fixtures\{ProtocoloFixture,ProcedimentoFixture,Atividade
 class TramiteProcessoBlocoDeTramiteRegrasTest extends FixtureCenarioBaseTestCase
 {
     public static $remetente;
-    public static $destinatario;
-    public static $processoTeste;
-    public static $documentoTeste;
     public static $objBlocoDeTramiteDTO;
 
     public static function setUpBeforeClass():void
@@ -33,23 +30,23 @@ class TramiteProcessoBlocoDeTramiteRegrasTest extends FixtureCenarioBaseTestCase
     {
         // Configuração do dados para teste do cenário
         self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
-        self::$processoTeste = $this->gerarDadosProcessoTeste(self::$remetente);
-        self::$documentoTeste = $this->gerarDadosDocumentoInternoTeste(self::$remetente);
+        $processoTeste = $this->gerarDadosProcessoTeste(self::$remetente);
+        $documentoTeste = $this->gerarDadosDocumentoInternoTeste(self::$remetente);
 
         // Cadastrar novo processo de teste
-        $objProtocoloDTO = $this->cadastrarProcessoFixture(self::$processoTeste);
+        $objProtocoloDTO = $this->cadastrarProcessoFixture($processoTeste);
 
         // Incluir e assinar documento no processo
         $dadosDocumentoDTO = [
             'IdProtocolo' => $objProtocoloDTO->getDblIdProtocolo(),
             'IdProcedimento' => $objProtocoloDTO->getDblIdProtocolo(),
-            'Descricao' => self::$documentoTeste['DESCRICAO'],
-            'IdHipoteseLegal' => self::$documentoTeste["HIPOTESE_LEGAL"],
-            'StaNivelAcessoGlobal' => self::$documentoTeste["RESTRICAO"],
-            'StaNivelAcessoLocal' => self::$documentoTeste["RESTRICAO"],
+            'Descricao' => $documentoTeste['DESCRICAO'],
+            'IdHipoteseLegal' => $documentoTeste["HIPOTESE_LEGAL"],
+            'StaNivelAcessoGlobal' => $documentoTeste["RESTRICAO"],
+            'StaNivelAcessoLocal' => $documentoTeste["RESTRICAO"],
         ];
 
-        if ($serieDTO = $this->buscarIdSerieDoDocumento(self::$documentoTeste['TIPO_DOCUMENTO'])) {
+        if ($serieDTO = $this->buscarIdSerieDoDocumento($documentoTeste['TIPO_DOCUMENTO'])) {
             $dadosDocumentoDTO['IdSerie'] = $serieDTO->getNumIdSerie();
         }
 
@@ -84,15 +81,14 @@ class TramiteProcessoBlocoDeTramiteRegrasTest extends FixtureCenarioBaseTestCase
     public function test_validar_mensagem_de_processo_bloqueado()
     {
         // Configuração do dados para teste do cenário
-        self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
-        self::$processoTeste = $this->gerarDadosProcessoTeste(self::$remetente);
-        self::$documentoTeste = $this->gerarDadosDocumentoInternoTeste(self::$remetente);
+        $processoTeste = $this->gerarDadosProcessoTeste(self::$remetente);
+        $documentoTeste = $this->gerarDadosDocumentoInternoTeste(self::$remetente);
 
         // Cadastrar novo processo de teste
-        $objProtocoloDTO = $this->cadastrarProcessoFixture(self::$processoTeste);
+        $objProtocoloDTO = $this->cadastrarProcessoFixture($processoTeste);
 
         // Incluir e assinar documento no processo
-        $this->cadastrarDocumentoInternoFixture(self::$documentoTeste, $objProtocoloDTO->getDblIdProtocolo());
+        $this->cadastrarDocumentoInternoFixture($documentoTeste, $objProtocoloDTO->getDblIdProtocolo());
 
         $bancoOrgaoA = new DatabaseUtils(CONTEXTO_ORGAO_A);
         $bancoOrgaoA->execute("update protocolo set sta_estado=? where id_protocolo=?;", array(4, $objProtocoloDTO->getDblIdProtocolo()));
@@ -125,15 +121,14 @@ class TramiteProcessoBlocoDeTramiteRegrasTest extends FixtureCenarioBaseTestCase
     public function test_validar_mensagem_de_processo_aberto_em_mais_de_uma_unidade()
     {
         // Configuração do dados para teste do cenário
-        self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
-        self::$processoTeste = $this->gerarDadosProcessoTeste(self::$remetente);
-        self::$documentoTeste = $this->gerarDadosDocumentoInternoTeste(self::$remetente);
+        $processoTeste = $this->gerarDadosProcessoTeste(self::$remetente);
+        $documentoTeste = $this->gerarDadosDocumentoInternoTeste(self::$remetente);
 
         // Cadastrar novo processo de teste
-        $objProtocoloDTO = $this->cadastrarProcessoFixture(self::$processoTeste);
+        $objProtocoloDTO = $this->cadastrarProcessoFixture($processoTeste);
 
         // Incluir e assinar documento no processo
-        $this->cadastrarDocumentoInternoFixture(self::$documentoTeste, $objProtocoloDTO->getDblIdProtocolo());
+        $this->cadastrarDocumentoInternoFixture($documentoTeste, $objProtocoloDTO->getDblIdProtocolo());
 
         // Acessar sistema do this->REMETENTE do processo
         $this->acessarSistema(self::$remetente['URL'], self::$remetente['SIGLA_UNIDADE'], self::$remetente['LOGIN'], self::$remetente['SENHA']);
@@ -156,6 +151,36 @@ class TramiteProcessoBlocoDeTramiteRegrasTest extends FixtureCenarioBaseTestCase
         );
         $this->assertStringContainsString(
             utf8_encode('Processo ' . $objProtocoloDTO->getStrProtocoloFormatado() . ' está aberto na(s) unidade(s): ' . self::$remetente['SIGLA_UNIDADE_SECUNDARIA']),
+            $mensagem
+        );
+    }
+
+    /**
+     * Teste pra validar a mensagem de processo sem documentos nao pode ser incluido em bloco
+     *
+     * @group envio
+     * @large
+     *
+     * @return void
+     */
+    public function test_validar_mensagem_de_processo_sem_documento()
+    {
+        // Configuração do dados para teste do cenário
+        $processoTeste = $this->gerarDadosProcessoTeste(self::$remetente);
+
+        $this->acessarSistema(self::$remetente['URL'], self::$remetente['SIGLA_UNIDADE'], self::$remetente['LOGIN'], self::$remetente['SENHA']);        
+        $strProtocoloTeste = $this->cadastrarProcesso($processoTeste);
+
+        $this->paginaBase->navegarParaControleProcesso();
+        $this->paginaTramiteEmBloco->selecionarProcessos([$strProtocoloTeste]);
+        $this->paginaTramiteEmBloco->selecionarTramiteEmBloco();
+        $this->paginaTramiteEmBloco->selecionarBloco(self::$objBlocoDeTramiteDTO->getNumId());
+        $this->paginaTramiteEmBloco->clicarSalvar();
+        sleep(2);
+
+        $mensagem = $this->paginaTramiteEmBloco->buscarMensagemAlerta();
+        $this->assertStringContainsString(
+            utf8_encode('Não é possível tramitar um processo sem documentos'),
             $mensagem
         );
     }
