916c77334f8ca9afa98e9f7edcd51b9e3ad9fe1d
Marcus Dias <31357298+marvdias@users.noreply.github.com>
2024-11-12 11:45:01 -0300

    fix: script instalacao 3.7.0 para postgresql (#706)

--- src/scripts/sei_atualizar_versao_modulo_pen.php
+++ src/scripts/sei_atualizar_versao_modulo_pen.php
@@ -2812,9 +2812,9 @@ class PenAtualizarSeiRN extends PenAtualizadorRN
         'id_unidade' => array($objMetaRestricaoBD->tipoNumero(), PenMetaBD::NNULLO),
         'id_unidade_rh' => array($objMetaRestricaoBD->tipoNumeroGrande(), PenMetaBD::NNULLO),
         'id_unidade_restricao' => array($objMetaRestricaoBD->tipoNumeroGrande(), PenMetaBD::NNULLO),
-        'nome_unidade_restricao' => array($objMetaRestricaoBD->tipoTextoVariavel(255), $SNULLO),
-        'id_unidade_rh_restricao' => array($objMetaRestricaoBD->tipoNumeroGrande(), $SNULLO),
-        'nome_unidade_rh_restricao' => array($objMetaRestricaoBD->tipoTextoVariavel(255), $SNULLO),
+        'nome_unidade_restricao' => array($objMetaRestricaoBD->tipoTextoVariavel(255), PenMetaBD::SNULLO),
+        'id_unidade_rh_restricao' => array($objMetaRestricaoBD->tipoNumeroGrande(), PenMetaBD::SNULLO),
+        'nome_unidade_rh_restricao' => array($objMetaRestricaoBD->tipoTextoVariavel(255), PenMetaBD::SNULLO),
       ),
       'pk' => array('cols' => array('id')),
       'fks' => array(
