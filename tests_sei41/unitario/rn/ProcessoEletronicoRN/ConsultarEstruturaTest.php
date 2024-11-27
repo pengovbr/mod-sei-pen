016d11a2764d5509ad09b45e777d03712f02db84
mauro <maurocosta.262017@gmail.com>
2024-11-27 18:29:03 -0300

    test: testes unitarios
    - teste unitarios de consulta estrutura, - teste unitário de consta estruturas, - reestruturacao de testes iniciais existentes, - correção de bug no docker-compose.yml de encoding, - inclusão de testdox para visualização de error e tempo de testes

---
+++ tests_sei41/unitario/rn/ProcessoEletronicoRN/ConsultarEstruturaTest.php
@@ -0,0 +1,108 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Classe de teste para o método listarRepositoriosDeEstruturas da classe ProcessoEletronicoRN.
+ * 
+ * Esta classe utiliza PHPUnit para verificar o comportamento do método listarRepositoriosDeEstruturas
+ * em diferentes cenários, garantindo que ele funcione conforme o esperado.
+ */
+class ConsultarEstruturaTest extends TestCase
+{
+    /**
+     * Mock da classe ProcessoEletronicoRN.
+     * 
+     * @var ProcessoEletronicoRN|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $mockService;
+
+    /**
+     * Configuração inicial do teste.
+     * 
+     * Este método cria um mock da classe ProcessoEletronicoRN e redefine
+     * o método 'get' para simular comportamentos durante os testes.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->mockService = $this->getMockBuilder(ProcessoEletronicoRN::class)
+                                  ->onlyMethods(['get'])
+                                  ->getMock();
+    }
+
+    public function testConsultarEstruturaListaSucesso()
+    {
+        $mockResponse = [
+          "numeroDeIdentificacaoDaEstrutura" => "159098",
+          "nome" => "Mauro ORG1 Filha",
+          "sigla" => "Mauro ORG1 Filha",
+          "ativo" => true,
+          "unidadeReceptora" => false,
+          "aptoParaReceberTramites" => true,
+          "codigoNoOrgaoEntidade" => "",
+          "codigoUnidadeReceptora" => "",
+          "tipoDeTramitacao" => 0,
+          "hierarquia" => [
+              [
+                  "numeroDeIdentificacaoDaEstrutura" => "152254",
+                  "nome" => "Órgão de Desenvolvimento ABC (FIRST) - ORGABC",
+                  "sigla" => "ORGABC"
+              ]
+          ]
+        ];
+
+        // Configura o mock para retornar a resposta
+        $this->mockService->expects($this->once())
+                          ->method('get')
+                          ->willReturn($mockResponse);
+
+        $resultado = $this->mockService->consultarEstrutura(159098, 152254, false);
+
+        $this->assertInstanceOf(EstruturaDTO::class, $resultado, 'O retorno deve ser uma instância da classe EstruturaDTO.');
+    }
+
+    public function testConsultarEstruturaListaBolRetornoRawTrueSucesso()
+    {
+        $mockResponse = [
+          "numeroDeIdentificacaoDaEstrutura" => "159098",
+          "nome" => "Mauro ORG1 Filha",
+          "sigla" => "Mauro ORG1 Filha",
+          "ativo" => true,
+          "unidadeReceptora" => false,
+          "aptoParaReceberTramites" => true,
+          "codigoNoOrgaoEntidade" => "",
+          "codigoUnidadeReceptora" => "",
+          "tipoDeTramitacao" => 0,
+          "hierarquia" => [
+              [
+                  "numeroDeIdentificacaoDaEstrutura" => "152254",
+                  "nome" => "Órgão de Desenvolvimento ABC (FIRST) - ORGABC",
+                  "sigla" => "ORGABC"
+              ]
+          ]
+        ];
+
+        // Configura o mock para retornar a resposta
+        $this->mockService->expects($this->once())
+                          ->method('get')
+                          ->willReturn($mockResponse);
+
+        $resultado = $this->mockService->consultarEstrutura(159098, 152254, true);
+
+        $this->assertInstanceOf(stdClass::class, $resultado, 'O retorno deve ser uma instância da classe EstruturaDTO.');
+    }
+
+    public function testConsultarEstruturaListaLancaExcecao()
+    {
+        $this->mockService->expects($this->once())
+                          ->method('get')
+                          ->willThrowException(new Exception('Erro na requisição'));
+
+        $this->expectException(InfraException::class);
+        $this->expectExceptionMessage('Falha na obtenção de unidades externas');
+
+        $this->mockService->consultarEstrutura(159098, 152254, false);
+    }
+}
