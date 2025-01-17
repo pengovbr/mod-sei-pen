fa6887c38770ba3798a9949c28507be68b81af12
mateus <3789823+mateussbh@users.noreply.github.com>
2025-01-17 15:02:35 -0300

    fix: Bloqueia trâmite de processos no SEI Federação.
    close #770 #758.

--- src/rn/ExpedirProcedimentoRN.php
+++ src/rn/ExpedirProcedimentoRN.php
@@ -1886,6 +1886,7 @@ class ExpedirProcedimentoRN extends InfraRN {
       $objProcedimentoDTO->retDblIdProcedimento();
       $objProcedimentoDTO->retNumIdHipoteseLegalProtocolo();
       $objProcedimentoDTO->retStrProtocoloProcedimentoFormatadoPesquisa();
+      $objProcedimentoDTO->retStrIdProtocoloFederacaoProtocolo();
 
       return $this->objProcedimentoRN->consultarRN0201($objProcedimentoDTO);
     }
@@ -2611,7 +2612,7 @@ class ExpedirProcedimentoRN extends InfraRN {
     private function validarProcedimentoCompartilhadoSeiFederacao(InfraException $objInfraException, $objProcedimentoDTO, $strAtributoValidacao) {
       $bolProcedimentoCompartilhado = false;
       $objProtocoloFederacaoDTO = new ProtocoloFederacaoDTO();
-      $objProtocoloFederacaoDTO->setStrProtocoloFormatadoPesquisa($objProcedimentoDTO->getStrProtocoloProcedimentoFormatadoPesquisa());
+      $objProtocoloFederacaoDTO->setStrIdProtocoloFederacao($objProcedimentoDTO->getStrIdProtocoloFederacaoProtocolo());
       $objProtocoloFederacaoDTO->retStrProtocoloFormatado();
 
       $objProtocoloFederacaoRN = new ProtocoloFederacaoRN();
