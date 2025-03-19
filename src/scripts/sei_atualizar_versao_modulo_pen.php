5aa75a645a7130d6919446cf4edf0073cf27d968
Carlos Vieira <3831408+caduvieira@users.noreply.github.com>
2025-03-19 14:13:09 -0300

    chore: permite subir de qualquer 3.8.X para o 4.0.0

--- src/scripts/sei_atualizar_versao_modulo_pen.php
+++ src/scripts/sei_atualizar_versao_modulo_pen.php
@@ -284,7 +284,7 @@ class PenAtualizarSeiRN extends PenAtualizadorRN
             $this->instalarV3070();
         case '3.7.0':
             $this->instalarV3080();
-        case '3.8.0':
+        case (preg_match('/3.8.*/', $name) ? true : false):
             $this->instalarV4000();
 
             break; // Ausência de [break;] proposital para realizar a atualização incremental de versões
