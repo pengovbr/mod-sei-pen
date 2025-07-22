f78e885adeb42508410b09f76fd3bf1496359c74
Jéssica Ferreira <gmjessicaferreira@gmail.com>
2025-07-22 10:04:39 -0300

    test: cancelamento tramite individual (#899)

--- tests_super/funcional/tests/CenarioBaseTestCase.php
+++ tests_super/funcional/tests/CenarioBaseTestCase.php
@@ -51,6 +51,7 @@ class CenarioBaseTestCase extends Selenium2TestCase
     protected $paginaEnvioParcialListar = null;
     protected $paginaPenHipoteseLegalListar = null;
     protected $paginaMapUnidades = null;
+    protected $paginaAgendamentos = null;
 
     public function setUpPage(): void
     {
@@ -81,6 +82,7 @@ class CenarioBaseTestCase extends Selenium2TestCase
         $this->paginaEnvioParcialListar = new PaginaEnvioParcialListar($this);
         $this->paginaPenHipoteseLegalListar = new PaginaPenHipoteseLegalListar($this);
         $this->paginaMapUnidades = new PaginaMapUnidades($this);
+        $this->paginaAgendamentos = new PaginaAgendamentos($this);
         $this->currentWindow()->maximize();
     }
 
