77b8d56a634c4ed51a4c39bb43846f8a18076ae1
Mauro Costa <83025710+Mauro1706@users.noreply.github.com>
2025-09-08 17:49:36 -0300

    Release/4.0.3 (#945)
    * feat: envio e recebimento do p7s do tramite de componentes digitais
    
    * feat: subindo versão do modulo pen para versão 4.0.3
    
    * feat: subindo versão do modulo pen para versão 4.0.3
    
    * fix: atualização de versão so pen no arquivo sip install

--- src/scripts/sip_atualizar_versao_modulo_pen.php
+++ src/scripts/sip_atualizar_versao_modulo_pen.php
@@ -1,7 +1,7 @@
 <?php
 
 // Identificação da versão do módulo mod-sei-pen. Este deve estar sempre sincronizado com a versão definida em PENIntegracao.php
-define("VERSAO_MODULO_PEN", "4.0.2");
+define("VERSAO_MODULO_PEN", "4.0.3");
 
 $dirSipWeb = !defined("DIR_SIP_WEB") ? getenv("DIR_SIP_WEB") ?: __DIR__ . "/../../web" : DIR_SIP_WEB;
 require_once $dirSipWeb . '/Sip.php';
